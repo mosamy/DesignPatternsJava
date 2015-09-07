f456ae3f7cc828fa0c0785b77fcd93194df9bb83
mohamed <m_raafat_samy@hotmail.com>
2015-09-06 19:30:00 -0500

    Final commit of homework

---
+++ IterationExcercise/src/GenericSumFunctor.java
@@ -0,0 +1,23 @@
+
+public class GenericSumFunctor<T>  implements IFunctor<T>{
+
+	T sum;
+	@Override
+	public void compute(T element) {
+		//sum =  sum + element;
+		
+	}
+
+	@Override
+	public T getValue() {
+		// TODO Auto-generated method stub
+		return sum;
+	}
+	
+}
+
+
+
+
+	
+
